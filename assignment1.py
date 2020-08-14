3311459ac0bf3c38ff4a7b0f9554416214793572
Sowmtih <sowmithnandanr@gmail.com>
2020-08-14 12:17:18 +0530

    multiplication function added

--- assignment1.py
+++ assignment1.py
@@ -2,15 +2,18 @@ def addition(a,b):
 	return a+b
 
 def subtraction(a,b):
-	return a-b;
+	return a-b
 
+def multiplication(a,b):
+	return a*b
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
-
+	print(multiplication(a,b))
+ 
 
 '''
 clone from github
